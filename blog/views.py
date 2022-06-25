66b221902940a084b315fe2760067a416ea40716
tyche0529 <tyche0529@gmail.com>
2022-06-26 01:12:15 +0900

    FBC로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    # db를 가져오고 오름차순으로 보일 수 있도록 함
+    posts = Post.objects.all().order_by('-pk')
+    return render(request, 'blog\index.html', {'posts' : posts})
