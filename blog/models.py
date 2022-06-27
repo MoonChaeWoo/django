7b7c4066358fd1f23c33ba026acc09ea95b4d41a
tyche0529 <tyche0529@gmail.com>
2022-06-27 23:07:31 +0900

    create의 DateTimeField 수정

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,7 @@ class Post(models.Model):
 
 # 업데이트 시간에 대한 필드 선언과 인자값으로 auto_now=True를 넣어준다.
 # 다시 저장할때 마다 그 시각이 저장되도록 한다.
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 # __str__은 관리자 페이지에서 제목과 유일키를 보여주는 기능을 함
